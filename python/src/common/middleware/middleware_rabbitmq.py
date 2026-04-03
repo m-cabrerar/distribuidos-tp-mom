65d03d6b0f35f97c5feaa8da6bcfc6ab020c5046
Mateo Cabrera <mcabrerar@fi.uba.ar>
2026-04-03 01:12:33 -0300

    middlewareQueue basico

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -6,7 +6,36 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        self.queue_name = queue_name
+        self.channel = None
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue=self.queue_name)
+
+    def send(self, message):
+        self.channel.basic_publish(exchange='',
+                      routing_key=self.queue_name,
+                      body=message)
+
+    def close(self):
+        if self.channel:
+            self.channel.close()
+    
+    def start_consuming(self, on_message_callback):
+        def callback(ch, method, properties, body):
+            def ack():
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+            def nack():
+                ch.basic_nack(delivery_tag=method.delivery_tag)
+            
+            on_message_callback(body, ack, nack)
+
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback)
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        self.channel.stop_consuming()
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
